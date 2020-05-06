5cb690068ee7c260f474bdf961f9c7652e45d161
my569 <1575883782@qq.com>
2020-05-07 04:12:19 +0800

    1、修改下一页的点击方式，避免被iee网站cookies设置的框框挡住;2、修复匹配错误，会议简称按全部匹配，比如简称‘DISA’的会议会与IS会议匹配成功;3、优化输出，运行时间格式化；添加更多输出信息；利用dataframe输出csv文件;

--- ieee.py
+++ ieee.py
@@ -1,6 +1,8 @@
 import time
 import timeit
 import os
+import re
+import pandas
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,7 +23,7 @@ def getInfo():
     showInfo_script = "function showInfo(node){                                                                                                    \
 	var paper_info = {};                                                                                                                           \
 	try {                                                                                                                                          \
-		home = 'ieeexplore.ieee.org';                                                                                                              \
+		home = 'https://ieeexplore.ieee.org';                                                                                                              \
 		paper_info['document_title'] = node.getElementsByTagName('h2')[0].innerText;                                                               \
 		paper_info['url'] = home + node.getElementsByTagName('h2')[0].getElementsByTagName('a')[0].getAttribute('href');                           \
 		paper_info['authors'] = node.getElementsByClassName('author')[0].innerText;                                                                \
@@ -53,19 +55,31 @@ def getInfo():
     return getInfoList();"
     return driver.execute_script(showInfo_script)
 
+def getShortName(str):
+    shortname = re.findall(r'[(](.*)[)]', str)
+    return shortname[0] if shortname else ''
+
 def fliter(ccf_list, datalist):
     res_list = []
     for i in range(len(datalist)):
         flag = False
         if 'publication_title' in datalist[i]:
             for y in ccf_list:
-                if datalist[i]["publication_title"].find(y[0]) != -1 or datalist[i]["publication_title"].find(y[0]) != -1:
+                shortname = getShortName(datalist[i]["publication_title"])
+                if datalist[i]["publication_title"].find(y[0]) != -1 or (shortname and y[1] and shortname == y[1]):
                     print(i,"success")
+                    print([datalist[i]["publication_title"],shortname], '匹配' , y)
+                    datalist[i]['match_longname'] = y[0] if y[0] else ''
+                    datalist[i]['match_shortname'] = y[1] if y[1] else ''
                     res_list.append(datalist[i])
                     flag = True
                     break
-        if not flag:
+            if not flag:
+                print(i,"fail")
+                print([datalist[i]["publication_title"],shortname], '匹配失败')
+        else:
             print(i,"fail")
+            print('该文章没有提供相应的出版物标题')
     return res_list
 
 import sqlite3
@@ -86,20 +100,19 @@ def get_ccf_list():
     conn.close()  
     return ccf_list
     
-    
-    
-    
 def nextPage():
     try:
         next_btn = driver.find_element_by_css_selector('.next-btn')
         print('页码：', driver.find_element_by_css_selector('a.active').text)
-        next_btn.click()
+        # next_btn.click()
+        driver.execute_script("document.getElementsByClassName('next-btn')[0].getElementsByTagName('a')[0].click()")
         return True
     except Exception as e:
         print('已经到达最后一页（即找不到下一页的按钮了）：', e)
         return False
 
-def waitPage(delay):# 等待页面渲染
+def waitPage(delay):
+    print('正在等待页面渲染成功...')
     try:
         WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.CLASS_NAME, "List-results")))
         print('List-results已加载')
@@ -121,22 +134,33 @@ def ReadPage():
     print('本页条目数：' ,len(datalist))
     return datalist  
 
-
+def pTime(start):
+    seconds = timeit.default_timer() - start
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    print ('Time:' , "%02d:%02d:%02d" % (h, m, s))
 
 
 start = timeit.default_timer()
-print('Time: ', timeit.default_timer() - start)   
+pTime(start)
 ccf_list = get_ccf_list()
 driver = webdriver.Chrome(options=brower_init())
 driver.get(test)
 waitPage(60)
 fliter(ccf_list, ReadPage())
 count = 0
-max = 1000
-result = []
+max = 10#检索成功的论文数
+result_all = []
+result = pandas.DataFrame(columns=("document_title","publication_title","url","authors","description","match_longname",'match_shortname',"abstract"))
 while nextPage() and count < max:
     waitPage(60)
-    result.append(fliter(ccf_list, ReadPage()))
-    count += 1
-    print('Time: ', timeit.default_timer() - start)  
+    for row in fliter(ccf_list, ReadPage()):
+        result = result.append(row, ignore_index=True) 
+        count += 1
+    pTime(start)
+print('检索结束，成功检索文章数：',len(result))
 print(result)
+save_directory = "E:/GraduationProject/result/"
+now = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime()) 
+filename = save_directory + ''+ now + '.csv'
+result.to_csv(filename, columns=['document_title','publication_title','url','match_longname','match_shortname'])
